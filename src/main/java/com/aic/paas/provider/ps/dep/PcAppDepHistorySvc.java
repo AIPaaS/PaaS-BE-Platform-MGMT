9e94ac56b635c98ed104b8b59bfe2b4a91ac3e53
bixy <bixy@bogon>
2016-03-07 18:10:58 +0800

    更新模型，添加history服务

---
+++ src/main/java/com/aic/paas/provider/ps/dep/PcAppDepHistorySvc.java
@@ -0,0 +1,21 @@
+package com.aic.paas.provider.ps.dep;
+
+import java.util.List;
+
+import com.aic.paas.provider.ps.bean.PcAppDepHistory;
+
+public interface PcAppDepHistorySvc {
+	/**
+	 * 保存获更新，判断主键ID[id]是否存在, 存在则更新, 不存在则插入
+	 * @param record : PcAppDepHistory数据记录
+	 * @return 当前记录主键[id]值
+	 */
+	public Long saveOrUpdate(PcAppDepHistory record);
+	
+	/**
+	 * 根据任务id获取app部署历史信息
+	 * @param taskId : Long任务ID
+	 * @return 任务[id]值
+	 */
+	public List<PcAppDepHistory> queryByTaskId(Long taskId);
+}
---
+++ src/main/java/com/aic/paas/provider/ps/dep/impl/PcAppDepHistorySvcImpl.java
@@ -0,0 +1,29 @@
+package com.aic.paas.provider.ps.dep.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.aic.paas.provider.ps.bean.CPcAppDepHistory;
+import com.aic.paas.provider.ps.bean.PcAppDepHistory;
+import com.aic.paas.provider.ps.db.PcAppDepHistoryDao;
+import com.aic.paas.provider.ps.dep.PcAppDepHistorySvc;
+
+public class PcAppDepHistorySvcImpl implements PcAppDepHistorySvc {
+
+	@Autowired
+	PcAppDepHistoryDao pcAppDepHistoryDao;
+
+	@Override
+	public Long saveOrUpdate(PcAppDepHistory record) {
+		return pcAppDepHistoryDao.save(record);
+	}
+
+	@Override
+	public List<PcAppDepHistory> queryByTaskId(Long taskId) {
+		CPcAppDepHistory cPcAppDepHistory = new CPcAppDepHistory();
+		cPcAppDepHistory.setTaskId(taskId);
+		return pcAppDepHistoryDao.selectList(cPcAppDepHistory, null);
+	}
+
+}
