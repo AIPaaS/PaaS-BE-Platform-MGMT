a24dd206978f67e11ba4a7224f58cde60c104b01
bixy <bixy@bogon>
2016-03-10 15:05:28 +0800

    add get tasks

--- src/main/java/com/aic/paas/provider/ps/dep/PcAppTaskSvc.java
+++ src/main/java/com/aic/paas/provider/ps/dep/PcAppTaskSvc.java
@@ -9,9 +9,8 @@ public interface PcAppTaskSvc {
 	/**
 	 * 保存获更新，判断主键ID[id]是否存在, 存在则更新, 不存在则插入
 	 * @param record : PcAppTask数据记录
-	 * @return 当前记录主键[id]值
 	 */
-	public Long saveOrUpdate(PcAppTask record);
+	public void save(PcAppTask record);
 	
 	public PcAppTask queryById(Long id);
 	
