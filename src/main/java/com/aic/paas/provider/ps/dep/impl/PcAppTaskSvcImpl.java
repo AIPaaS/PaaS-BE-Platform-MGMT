41573f79e9dcbc31a60a7de244efefcf23ab0aec
bixy <bixy@bogon>
2016-03-11 11:20:20 +0800

    return task info add versionNo

--- src/main/java/com/aic/paas/provider/ps/dep/impl/PcAppTaskSvcImpl.java
+++ src/main/java/com/aic/paas/provider/ps/dep/impl/PcAppTaskSvcImpl.java
@@ -1,13 +1,19 @@
 package com.aic.paas.provider.ps.dep.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.aic.paas.provider.ps.bean.CPcAppTask;
 import com.aic.paas.provider.ps.bean.PcAppTask;
+import com.aic.paas.provider.ps.bean.PcAppVersion;
 import com.aic.paas.provider.ps.db.PcAppDao;
 import com.aic.paas.provider.ps.db.PcAppDepHistoryDao;
 import com.aic.paas.provider.ps.db.PcAppTaskDao;
+import com.aic.paas.provider.ps.db.PcAppVersionDao;
 import com.aic.paas.provider.ps.dep.PcAppTaskSvc;
+import com.aic.paas.provider.ps.dep.bean.PcAppTaskInfo;
 import com.binary.core.util.BinaryUtils;
 import com.binary.jdbc.Page;
 
@@ -19,6 +25,8 @@ public class PcAppTaskSvcImpl implements PcAppTaskSvc {
 	PcAppDao pcAppDao;
 	@Autowired
 	PcAppDepHistoryDao pcAppDepHistoryDao;
+	@Autowired
+	PcAppVersionDao pcAppVersionDao;
 
 	@Override
 	public void save(PcAppTask record) {
@@ -42,6 +50,26 @@ public class PcAppTaskSvcImpl implements PcAppTaskSvc {
 	@Override
 	public void update(PcAppTask record) {
 		pcAppTaskDao.updateById(record, record.getId());
+	}
+
+	@Override
+	public Page<PcAppTaskInfo> queryPcAppTaskInfo(Integer pageNum, Integer pageSize, CPcAppTask cdt, String orders) {
+		Page<PcAppTask> pageTask = pcAppTaskDao.selectPage(pageNum, pageSize, cdt, orders);
+		List<PcAppTaskInfo> pcAppTaskInfoList = new ArrayList<>();
+		for (PcAppTask task : pageTask.getData()) {
+			PcAppTaskInfo pcAppTaskInfo = new PcAppTaskInfo();
+			pcAppTaskInfo.setPcAppTask(task);
+			PcAppVersion pcAppVersion = pcAppVersionDao.selectById(task.getAppVnoId());
+			pcAppTaskInfo.setVersionNo(pcAppVersion.getVersionNo());
+			pcAppTaskInfoList.add(pcAppTaskInfo);
+		}
+		Page<PcAppTaskInfo> result = new Page<>();
+		result.setData(pcAppTaskInfoList);
+		result.setPageNum(pageTask.getPageNum());
+		result.setPageSize(pageTask.getPageSize());
+		result.setTotalPages(pageTask.getTotalPages());
+		result.setTotalRows(pageTask.getTotalRows());
+		return null;
 	}
 
 }
