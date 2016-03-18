fae76595d1519f123601ceb23612669d3e19ade8
zhangqiang7 <zhangqiang7@aisainfo.com>
2016-03-18 20:56:53 +0800

    dao

---
+++ src/main/java/com/aic/paas/provider/ps/dep/PcAppImageDao.java
@@ -0,0 +1,17 @@
+package com.aic.testcode.db;
+
+
+import com.binary.framework.dao.Dao;
+import com.aic.testcode.bean.CPcAppImage;
+import com.aic.testcode.bean.PcAppImage;
+
+
+/**
+ * 应用镜像表[PC_APP_IMAGE]数据访问对象
+ */
+public interface PcAppImageDao extends Dao<PcAppImage, CPcAppImage> {
+
+
+}
+
+
